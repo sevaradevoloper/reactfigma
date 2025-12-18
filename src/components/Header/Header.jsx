b629632a6756ec4843e261e1444deae5198cfa41
sevara128@icloud.com <sevara128@icloud.com>
2025-12-18 15:50:28 +0300

    uchchi commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // 1. useState-ni import qilish shart
+import React, { useState } from 'react';
 import './Header.css'; 
 import Logo from "./image/logo.svg"; 
 
@@ -19,8 +19,6 @@ function Header() {
               <img src={Logo} alt="logo" width="50" />Artificial Intelligence
             </a>
           </div>
-
-          {/* 3. Dinamik klass qo'shish: isOpen true bo'lsa 'active' klassi qo'shiladi */}
           <nav className={`header__nav ${isOpen ? "active" : ""}`} id='nav-links'>
             <ul className="header__list">
               <li><a href="#" className="header__link">Home</a></li>
@@ -30,8 +28,6 @@ function Header() {
             </ul>
             <button className="header__btn">Sign In</button>
           </nav>
-
-          {/* 4. Hamburger tugmasiga ham 'open' klassini berish (X shakliga aylanishi uchun) */}
           <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
             <div></div>
             <div></div>
--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -6,7 +6,7 @@ import Swiper1 from "./image/swiper1.svg";
 import Swiper2 from "./image/swiper2.svg";
 import Swiper3 from "./image/swiper3.svg";
 import Swiper4 from "./image/swiper4.svg";
-
+import Swiper5 from "./image/swiper5.svg";
 function Swiper() {
   return (
     <>
@@ -15,8 +15,28 @@ function Swiper() {
           <div className="swiper__container">
             <ul className="swiper__list">
                 <li className="swiper__items">
-                    <img src="{Swiper1}" alt="Swiperimages" />
+                    <img src={Swiper1} alt="Swiperimages" />
                     <h4>Naxly as the Winners in Global Agency Awards</h4>
+                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+                </li>
+                <li className="swiper__items">
+                    <img src={Swiper2} alt="Swiperimages" />
+                    <h4>Expert Prespective Agency Awards</h4>
+                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+                </li>
+                <li className="swiper__items">
+                    <img src={Swiper3} alt="Swiperimages" />
+                    <h4>Business Prespective Global Agency Awards</h4>
+                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+                </li>
+                <li className="swiper__items">
+                    <img src={Swiper4} alt="Swiperimages" />
+                    <h4>Value for Results in Global Agency Awards</h4>
+                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+                </li>
+                <li className="swiper__items">
+                    <img src={Swiper5} alt="Swiperimages" />
+                    <h4>Global Experience in Agency Awards</h4>
                     <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
                 </li>
             </ul>
