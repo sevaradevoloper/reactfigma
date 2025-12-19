5f06f0043b405a52f889a039d12870fb0e0d82a7
sevara128@icloud.com <sevara128@icloud.com>
2025-12-19 18:34:57 +0300

    tortinchi commit

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -1,52 +1,66 @@
 import React from 'react'; 
+import Slider from "react-slick";
+
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import './Swiper.css'; 
 
 
 import Swiper1 from "./image/swiper1.svg";
 import Swiper2 from "./image/swiper2.svg";
 import Swiper3 from "./image/swiper3.svg";
-import Swiper4 from "./image/swiper4.svg";
+import Swiper4 from "./image/swiper5.svg";
 import Swiper5 from "./image/swiper5.svg";
-function Swiper() {
-  return (
-    <>
-      <section className="swiper">
-        <div className="container">
-          <div className="swiper__container">
-            <ul className="swiper__list">
-                <li className="swiper__items">
-                    <img src={Swiper1} alt="Swiperimages" />
-                    <h4>Naxly as the Winners in Global Agency Awards</h4>
-                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
-                </li>
-                <li className="swiper__items">
-                    <img src={Swiper2} alt="Swiperimages" />
-                    <h4>Expert Prespective Agency Awards</h4>
-                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
-                </li>
-                <li className="swiper__items">
-                    <img src={Swiper3} alt="Swiperimages" />
-                    <h4>Business Prespective Global Agency Awards</h4>
-                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
-                </li>
-                <li className="swiper__items">
-                    <img src={Swiper4} alt="Swiperimages" />
-                    <h4>Value for Results in Global Agency Awards</h4>
-                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
-                </li>
-                <li className="swiper__items">
-                    <img src={Swiper5} alt="Swiperimages" />
-                    <h4>Global Experience in Agency Awards</h4>
-                    <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
-                </li>
-            </ul>
-          </div>
-        </div>
-      </section>
 
-      
-    </>
+
+export default function Swiper() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true, // Avtomatik aylanishni yoqing (tekshirish uchun)
+    autoplaySpeed: 2000,
+  };
+
+  return (
+    <section className="swiper-section">
+      <div className="container">
+        {/* Slider atrofida qo'shimcha div bo'lgani ma'qul */}
+        <div className="slider-wrapper">
+          <Slider {...settings}>
+            <div key="1" className="swiper__item">
+              <img src={Swiper1} alt="AI" />
+              <h4>Naxly as the Winners in Global Agency Awards</h4>
+              <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+            <div key="2" className="swiper__item">
+              <img src={Swiper2} alt="AI" />
+              <h4>Expert Prespective Agency Awards</h4>
+              <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+            <div key="3" className="swiper__item">
+              <img src={Swiper3} alt="AI" />
+              <h4>Business Prespective Global Agency Awards</h4>
+              <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+            <div key="1" className="swiper__item">
+              <img src={Swiper4} alt="AI" />
+              <h4>Value for Results in Global Agency Awards</h4>
+              <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+            <div key="2" className="swiper__item">
+              <img src={Swiper5} alt="AI" />
+              <h4>Value for Results in Global Agency Awards</h4>
+              <p>Lorem ipsum is placeholder text commonly used in print, and publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+            
+          </Slider>
+        </div>
+      </div>
+    </section>
   );
 }
-
-export default Swiper;
